186a21ccc4927acee1498c75999c17712c7b309f
ramon.nascimento <ramon.nascimento@PC-RAMON>
2020-07-02 15:53:54 -0300

    Crud módulos, inserir, listar. Concluidos.

--- EstudoR2/Controllers/ModuloController.cs
+++ EstudoR2/Controllers/ModuloController.cs
@@ -3,15 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EstudoR2.Context;
+using EstudoR2.Models;
+using EstudoR2.ViewModels;
 
 namespace EstudoR2.Controllers
 {
     public class ModuloController : Controller
     {
-        // GET: Modulo
-        public ActionResult ListarModulo()
+        public ActionResult ListarModulos()
         {
-            return View();
+
+            using (var db = new R2Context())
+            {
+                var listaModulos = db.Modulos.ToList();
+                return View(listaModulos);
+            }
+
+        }
+
+        public ActionResult InserirModulo()
+        {
+            using (var db = new R2Context())
+            {
+                var listaCursosDisponiveis = db.Cursos.Select(x => new IdValorViewModel()
+                {
+                    Id = x.IdCurso,
+                    Valor = x.NomeCurso
+                }).ToList();
+
+                var mooduloViewModel = new ListaCursosViewModel()
+                {
+                    CursosDisponiveis = listaCursosDisponiveis
+                };
+                return View(mooduloViewModel);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult InserirModuloAction(string nomeModulo, int IdCursoSelecionado)
+        {
+            Modulo m1 = new Modulo();
+            m1.NomeModulo = nomeModulo;
+            m1.IdCurso = IdCursoSelecionado;
+
+            using (var db = new R2Context())
+            {
+                db.Modulos.Add(m1);
+                db.SaveChanges(); 
+                return RedirectToAction("ListarModulos");
+            }
+
+        }
+
+        public ActionResult ApagarModulo(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult EditarModulo(string s)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ EstudoR2/ViewModels/ListaCursosViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudoR2.ViewModels
+{
+    public class ListaCursosViewModel
+    {
+
+        public ListaCursosViewModel()
+        {
+            CursosDisponiveis = new List<IdValorViewModel>();
+        }
+
+        public int IdCursoSelecionado { get; set; }
+        public IList<IdValorViewModel> CursosDisponiveis{ get; set; }
+
+    }
+}
