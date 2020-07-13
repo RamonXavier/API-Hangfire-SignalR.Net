c1fc608eaeaa8547a688abaac4b681670e0ee535
ramon.nascimento <ramon.nascimento@PC-RAMON>
2020-07-13 16:54:28 -0300

    Criado Central de avisos, com elayoute de  chat

---
+++ EstudoR2/Controllers/CentralAvisoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EstudoR2.Controllers
+{
+    public class CentralAvisoController : Controller
+    {
+        public ActionResult CentralAviso()
+        {
+            return View();
+        }
+    }
+}
